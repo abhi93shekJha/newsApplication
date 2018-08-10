e1148df4811620f15421c80064a6701995bbb04d
PRASOON PV <prasoonpv99@gmail.com>
2018-08-10 19:17:18 +0530

    compleate navigation drawer, working on login screen

--- app/src/main/java/com/gsatechworld/gugrify/view/dashboard/DashboardActivity.java
+++ app/src/main/java/com/gsatechworld/gugrify/view/dashboard/DashboardActivity.java
@@ -1,24 +1,26 @@
 package com.gsatechworld.gugrify.view.dashboard;
 
-import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -30,7 +32,8 @@ import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 
-public class DashboardActivity extends AppCompatActivity implements OnRecyclerItemClickListener {
+public class DashboardActivity extends AppCompatActivity implements OnRecyclerItemClickListener,
+        NavigationView.OnNavigationItemSelectedListener {
 
     private AutoScrollViewPager viewPager;
     private ViewPagerAdapter mAdapter;
@@ -40,6 +43,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
+
     private LinearLayout pager_indicator;
     private ArrayList<SectionDataModel> allSampleData;
     private SectionDataModel sectionModel;
@@ -52,9 +56,34 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
         setSupportActionBar(toolbar);
 
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setDisplayShowCustomEnabled(true);
+            getSupportActionBar().setCustomView(R.layout.custom_actionbar);
+            View view = getSupportActionBar().getCustomView();
         }
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //After instantiating your ActionBarDrawerToggle
+        toggle.setDrawerIndicatorEnabled(false);
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.mipmap.logo, getTheme());
+        toggle.setHomeAsUpIndicator(drawable);
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerVisible(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         // clear FLAG_TRANSLUCENT_STATUS flag:
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -64,7 +93,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
 
         // finally change the color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.ststusbar_color));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.ststusbar_color));
         }
 
         ButterKnife.bind(this);
@@ -84,7 +113,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
         dotscount = viewPagerAdapter.getCount();
         dots = new ImageView[dotscount];
 
-        for(int i = 0; i < dotscount; i++){
+        for (int i = 0; i < dotscount; i++) {
 
             dots[i] = new ImageView(this);
             dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
@@ -109,7 +138,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
             @Override
             public void onPageSelected(int position) {
 
-                for(int i = 0; i< dotscount; i++){
+                for (int i = 0; i < dotscount; i++) {
                     dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
                 }
 
@@ -134,9 +163,43 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
         recyclerView.setAdapter(adapter);
         ViewCompat.setNestedScrollingEnabled(recyclerView, false);
         //adapter.setItems(allSampleData);
+
+        // create nav item list
+        ArrayList<NavItemModel> navItemModelArrayList = getNavItemList();
+
+        RecyclerView recycler_nav_item = (RecyclerView) findViewById(R.id.recycler_nav_item);
+        recycler_nav_item.setHasFixedSize(true);
+        RecyclerViewNavAdapter navAdapter = new RecyclerViewNavAdapter(navItemModelArrayList, this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recycler_nav_item.setLayoutManager(layoutManager);
+        recycler_nav_item.setAdapter(navAdapter);
+        ViewCompat.setNestedScrollingEnabled(recycler_nav_item, false);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
     }
 
-    private ArrayList<PlayListItemModel> createPlaylistData(){
+    private ArrayList<NavItemModel> getNavItemList() {
+        ArrayList<NavItemModel> navItemModelList = new ArrayList<>();
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        navItemModelList.add(new NavItemModel(R.drawable.ic_menu_orange, "Breaking news 1",
+                "8"));
+        return navItemModelList;
+    }
+
+    private ArrayList<PlayListItemModel> createPlaylistData() {
         ArrayList<PlayListItemModel> playListItemModelArrayList = new ArrayList<>();
         playListItemModelArrayList.add(new PlayListItemModel("99 +", "", "aaaaa", R.drawable.food6));
         playListItemModelArrayList.add(new PlayListItemModel("25", "", "bbbbb", R.drawable.food2));
@@ -146,7 +209,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
         return playListItemModelArrayList;
     }
 
-    private ArrayList<LatestNewItemModel> createLatestNews(){
+    private ArrayList<LatestNewItemModel> createLatestNews() {
         ArrayList<LatestNewItemModel> latestNewItemModelArrayList = new ArrayList<>();
         latestNewItemModelArrayList.add(new LatestNewItemModel("Usa", "", "aaaaa", R.drawable.country1));
         latestNewItemModelArrayList.add(new LatestNewItemModel("India", "", "bbbbb", R.drawable.country6));
@@ -157,7 +220,7 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
         return latestNewItemModelArrayList;
     }
 
-    private ArrayList<OtherNewsItemModel> createOtherNews(){
+    private ArrayList<OtherNewsItemModel> createOtherNews() {
         ArrayList<OtherNewsItemModel> otherNewsItemModelArrayList = new ArrayList<>();
         otherNewsItemModelArrayList.add(new OtherNewsItemModel("AA", "", "AAAA", R.drawable.road1));
         otherNewsItemModelArrayList.add(new OtherNewsItemModel("BB", "", "BBBB", R.drawable.road2));
@@ -173,22 +236,22 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
 //            ArrayList<SingleItemModel> singleItemModels = new ArrayList<>();
 //            dmSingle.setAllItemInSection(singleItemModels);
 //            allSampleData.add(dmSingle);
-            sectionModel = new SectionDataModel("LATEST NEWS");
-            sectionModel.setLatestNewItemModelArrayList(createLatestNews());
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("Play List");
-            sectionModel.setPlayListItemModelArrayList(createPlaylistData());
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("AA", R.drawable.road1);
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("BB", R.drawable.road2);
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("CC", R.drawable.road3);
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("DD", R.drawable.road4);
-            allSampleData.add(sectionModel);
-            sectionModel = new SectionDataModel("EE", R.drawable.road5);
-            allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("LATEST NEWS");
+        sectionModel.setLatestNewItemModelArrayList(createLatestNews());
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("Play List");
+        sectionModel.setPlayListItemModelArrayList(createPlaylistData());
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("AA", R.drawable.road1);
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("BB", R.drawable.road2);
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("CC", R.drawable.road3);
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("DD", R.drawable.road4);
+        allSampleData.add(sectionModel);
+        sectionModel = new SectionDataModel("EE", R.drawable.road5);
+        allSampleData.add(sectionModel);
     }
 
     @Override
@@ -210,6 +273,41 @@ public class DashboardActivity extends AppCompatActivity implements OnRecyclerIt
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
