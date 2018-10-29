d1cdafb34c2d22ea1a07b483868d1ae0a50f383f
Ashish Pandey <ashpandey.777@gmail.com>
2018-10-29 17:38:11 +0530

    SectionListData Adapter And activity_report_post is done the UI

--- app/src/main/java/com/gsatechworld/gugrify/view/dashboard/SectionListDataAdapter.java
+++ app/src/main/java/com/gsatechworld/gugrify/view/dashboard/SectionListDataAdapter.java
@@ -2,6 +2,7 @@ package com.gsatechworld.gugrify.view.dashboard;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
